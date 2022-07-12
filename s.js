7fe2f66cc04f5297d82494720676a113db664323
sh1ro <nepnepn9p@gmail.com>
2022-07-12 15:00:36 +0300

    ispr format i cpukol

--- s.js
+++ s.js
@@ -1,14 +1,15 @@
 const cluster = require('cluster');
 const http = require('http');
 const fs = require('fs');
-const numCPU = 2; //na moem noyte
+// const numCPU = 2; //na moem noyte
 var url = require("url");
+var os = require("os");
 const host = '127.0.0.1';
 const port = 8000;
 
 
 if (cluster.isMaster) {
-  for (let i = 0; i < numCPU; i++) cluster.fork();
+  for (let i = 0; i < os.cpus().length; i++) cluster.fork();
   cluster.on('exit', (worker, code) => {
     console.log( `Worker ${worker.id} finished. Exit code: ${code}`);
   });
@@ -40,10 +41,16 @@ if (cluster.isMaster) {
     });
 
         var date = new Date();
-        var curren_date = date.getFullYear() + '-' +
-        (date.getMonth() + 1) + '-' + (date.getDate());
-        var current_time = (date.getHours()) + ':' +
-        (date.getMinutes()) + ':' + (date.getSeconds());
+        var date1 = date.getDate();
+        if (date1 < 10){date1 = '0' + date1;}
+        var curren_date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + (date1);
+        var hour1 = date.getHours();
+        if (hour1 < 10){hour1 = '0' + hour1;}
+        var min1 = date.getMinutes();
+        if (min1 < 10){min1 = '0' + min1;}
+        var sec1 = date.getSeconds();
+        if (sec1 < 10){sec1 = '0' + sec1;}
+        var current_time = (hour1) + ':' + (min1) + ':' + (sec1);
         var inf = '[' + curren_date + ' ' + current_time + '] ' + ip + ' ' + ` ${client_req.method}` + ' ' + client_req.url;
          fs.appendFile('log.txt', inf + '\n', function(err) {
       if (err) {
@@ -52,6 +59,5 @@ if (cluster.isMaster) {
         console.log(`${inf} saved!`);
       }
 	});
-
 }
 }
